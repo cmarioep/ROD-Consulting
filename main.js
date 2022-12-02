6c48c74dc2eea74c40eca2cace66aad897e0e064
cmarioep <cmescandonpardo@gmail.com>
2022-12-02 17:00:08 -0500

    Fix: import js libraries from main

--- main.js
+++ main.js
@@ -3,13 +3,15 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+import { Fancybox } from "@fancyapps/ui";
+import { Carousel } from './node_modules/@fancyapps/ui/src/Carousel/Carousel.js'
 
 
 const mainCarousel = new Carousel(document.querySelector("#mainCarousel"), {
   // Your options
 });
 
-AOS.init({  
+AOS.init({
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
